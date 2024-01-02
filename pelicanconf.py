3df3cea93b01e3f0389ab3e78c8472353153b4f8
Fei Zhao <2550688560@qq.com>
2024-01-02 19:16:40 +0800

    update

--- pelicanconf.py
+++ pelicanconf.py
@@ -44,11 +44,11 @@ DISPLAY_CATEGORIES_ON_MENU = False
 OUTPUT_PATH = 'docs/'
 STATIC_PATHS = [
     "images",
-    "static/favicon.png",
-    "static/CNAME",
+    "static",
     "pdfs"
 ]
 EXTRA_PATH_METADATA = {
     "static/CNAME": {"path": "CNAME"},
     "static/favicon.png": {"path": "favicon.png"},
+    "static/favicon.ico": {"path": "favicon.ico"},
 }
